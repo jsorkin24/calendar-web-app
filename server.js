a8f028a2e8a61c6dc8246724a70ff3dc70aebc2f
Jane <sorkinjane@gmail.com>
2017-10-30 14:59:11 -0700

    mongodb

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8080
 
 // mpromise is depreciated, use native es6 Promise
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
     useMongoClient: true
 })
 
